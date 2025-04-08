0a624d1f0652ff0200dc449ce40112f2dee1f63b
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-04-08 19:48:14 +0530

    Create GFG._Rearrange_a_Linked_List.cpp

---
+++ LinkedList/GFG._Rearrange_a_Linked_List.cpp
@@ -0,0 +1,30 @@
+/* Node structure
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void rearrangeEvenOdd(Node *head) {
+        Node *odd = head;
+        Node *even = head->next;
+        Node *evencopy = even;
+        
+        while(odd->next && even->next){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evencopy;
+    }
+};
