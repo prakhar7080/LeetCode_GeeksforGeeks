7fed923720cf4ec74feee2fe2ccd3617ed4fa9a7
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-04-12 23:20:22 +0530

    Create 921_Minimum_add_to_parenthesis_valid.cpp

---
+++ Stack/921_Minimum_add_to_parenthesis_valid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int i;
+        for(i=0;i<s.size();i++){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else{
+                if(st.top() == '(' && s[i] == ')'){
+                    st.pop();
+                }
+                else{
+                    st.push(s[i]);
+                }
+            }
+        }
+        return st.size();
+    }
+};
