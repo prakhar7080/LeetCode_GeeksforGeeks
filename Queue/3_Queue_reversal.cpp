f3f2c43bb734a27c5293e266a17b6bcf9aceb03f
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-04-23 23:20:54 +0530

    Update 3_Queue_reversal.cpp

--- Queue/3_Queue_reversal.cpp
+++ Queue/3_Queue_reversal.cpp
@@ -1,16 +1,15 @@
 class Solution {
   public:
     queue<int> reverseQueue(queue<int> &q) {
-        queue<int>ans;
         stack<int>s;
         while(!q.empty()){
             s.push(q.front());
             q.pop();
         }
         while(!s.empty()){
-            ans.push(s.top());
+            q.push(s.top());
             s.pop();
         }
-        return ans;
+        return q;
     }
 };
