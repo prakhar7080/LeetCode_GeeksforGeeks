7622f8d97d854eb7b50e2cf9455a65651e42b652
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-05-27 21:45:33 +0530

    Update sum_Array.cpp

--- Recursion/sum_Array.cpp
+++ Recursion/sum_Array.cpp
@@ -13,3 +13,18 @@ int main(){
     sum(arr,0,5,s);
     cout<<"Sum : "<<s<<endl;
 }
+/*
+#include<iostream>
+using namespace std;
+int sum(int arr[],int i, int n){
+    if(i>=n){
+        return 0;
+    }
+    return arr[i]+sum(arr,i+1,n);
+}
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int s = sum(arr,0,5);
+    cout<<"Sum : "<<s<<endl;
+}
+*/
