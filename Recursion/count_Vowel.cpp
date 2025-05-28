7e1e5d77f8077cc9e8918a444ed6993787ea629f
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-05-28 17:17:42 +0530

    Create count_Vowel.cpp

---
+++ Recursion/count_Vowel.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int count(string str, int i){
+    if(i>str.length()-1){
+        return 0;
+    }
+    char ch = tolower(str[i]);
+    if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
+        return 1 + count(str,i+1);
+    }
+    return count(str,i+1);
+}
+int main(){
+    string str;
+    cin>>str;
+    cout<<"No of vowels : "<<count(str,0);
+}
